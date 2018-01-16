dc8c65dc36435fa279eb014ee6d78350ea308012
Eric Oeur <ericeoeur@gmail.com>
2018-01-16 17:19:55 -0600

    latest updates with the movie omdb request

--- liri.js
+++ liri.js
@@ -3,19 +3,82 @@ var keys = require("./keys.js");
 var fs = require("fs"); //remember to "npm install fs" on command line
 var request = require("request"); //remember to "npm install request" on command line
 var twitter = require("twitter"); //remember to "npm install twitter" on command line
-var spotify = require ("spotify"); //remember to "npm install spotify" on command line
-var liri= process.argv[2]; //This holds the first command line input
-console.log ("This is what was inputted: " + liri);
+var spotify = require("spotify"); //remember to "npm install spotify" on command line
+var liri = process.argv[2]; //This holds the first command line input
+console.log("This is what was inputted: " + liri);
 
 
 
 
 //add command line calls to functions here
-switch(liri) {
+switch (liri) {
     case "my-tweets": myTweets(); break;
-    //case "spotify-shit"
+    case "spotify-this-song": spotifySong(); break; 
+    case "movie-this": simpleRequest(); break; 
 
 };
+
+
+
+function simpleRequest() {
+  
+    let movie = process.argv[3];
+    if (!movie) {
+        movie = "mr nobody";
+    }
+    params = movie
+
+
+    request("http://www.omdbapi.com/?apikey=9a95b62e&t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            let movieObject = JSON.parse(body);
+            //console.log(movieObject); // Show the text in the terminal
+            console.log("***********************" + movieObject.Title + "***********************" + "\r\n" +
+                "Title: " + movieObject.Title + "\r\n" +
+                "Year: " + movieObject.Year + "\r\n" +
+                "Imdb Rating: " + movieObject.imdbRating + "\r\n" +
+                "Rotten Tomatoes Rating: " + movieObject.tomatoRating + "\r\n" +
+                "Country: " + movieObject.Country + "\r\n" +
+                "Language: " + movieObject.Language + "\r\n" +
+                "Plot: " + movieObject.Plot + "\r\n" +
+                "Actors: " + movieObject.Actors + "\r\n" +
+                "******************************************************************************");
+
+
+        } else {
+            console.log("Error :" + error);
+            return;
+        }
+    });
+
+};
+
+
+
+
+function spotifySong () {
+    console.log("Spotify ID: " + keys.Spotify.id);
+   
+}
+   
+   
+
+   
+   /* //console.log(keys.Spotify.id); 
+
+   let spotify = {
+        id: keys.Spotify.id,
+        secret: keys.Spotify.secret,
+    };
+
+*/
+     
+    
+
+
+
+
 
 
 function myTweets() {
@@ -26,14 +89,14 @@ function myTweets() {
         consumer_key: keys.Twitter.consumer_key,
         consumer_secret: keys.Twitter.consumer_secret,
         access_token_key: keys.Twitter.access_token_key,
-        access_token_secret: keys.Twitter.access_token_secret, 
+        access_token_secret: keys.Twitter.access_token_secret,
     });
 
     //Command Line input for twitter username
-    var twitterUsername = process.argv[3];
+    let twitterUsername = process.argv[3];
 
     //If no username was entered default to your own twitter handle
-    if(!twitterUsername){
+    if (!twitterUsername) {
         twitterUsername = "ericetwice";
     }
 
@@ -42,23 +105,22 @@ function myTweets() {
         screen_name: twitterUsername
     };
 
-
     //Twitter API Documentation -- pulling from user_timeline. 
     // use this as reference: https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
-    client.get("statuses/user_timeline/", twitterHandle, function(error, data, response){
+    client.get("statuses/user_timeline/", twitterHandle, function (error, data, response) {
         if (!error) {
-            for(let i = 1; i < 20; i++) {
-               
-                let results = 
-                data[i].text + "\r\n\r\n" + 
-                "Tweeted on: " + data[i].created_at + "\r\n\r\n" + 
-                "*********************** @" + data[i].user.screen_name + "'s Tweet Number: "  + i + " *********************** " + "\r\n";
+            for (let i = 0; i < 20; i++) {
+                let tweetCount = i+1; 
+                let results =
+                    data[i].text + "\r\n\r\n" +
+                    "Tweeted on: " + data[i].created_at + "\r\n\r\n" +
+                    "*********************** @" + data[i].user.screen_name + "'s Tweet Number: " + tweetCount + " *********************** " + "\r\n";
                 console.log(results);
-                 //console.log(response); //this shows everything from user_timeline
-             
+                //console.log(response); //this shows everything from user_timeline
+
             }
-        }  else {
-            console.log("There was an error. Error code: "+ error);
+        } else {
+            console.log("There was an error. Error code: " + error);
             return;
         }
     });
